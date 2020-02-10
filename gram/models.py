0e25a049dc5781be158dde993d7c6aa17a727379
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2020-02-10 18:26:32 +0300

    saving comments

--- gram/models.py
+++ gram/models.py
@@ -68,3 +68,6 @@ class Comment(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     image = models.ForeignKey(Image,on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.comments
+    
