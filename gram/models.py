fcb4d9d882c85177300b61500cbb2916ea16f348
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2020-02-10 18:25:31 +0300

    comment model

--- gram/models.py
+++ gram/models.py
@@ -61,3 +61,10 @@ class Image(models.Model):
     def get_image_by_id(cls,id):
         image = Image.objects.filter(id=Image.id)
         return image
+
+class Comment(models.Model):
+    comments = models.CharField(max_length=60,null=True)
+    comment_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    
